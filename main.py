e290ebb4ccc43c86f3061e8048a4a66e8a5dfcb5
kamela <kamelawaelelgadi889@gmail.com>
2025-10-02 19:49:55 +0100

    تعديل في الميين

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from routers import register ,login
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
-#app.include_router(register.router)
-#app.include_router(login.router)
-@app.get("/")
-def home():
-   return {"message":"welcom to my API 🎉"}
-@app.get("/test")
-def test():
-   return {"message":"ok"}
+app.include_router(register.router)
+app.include_router(login.router)
+app.add_middleware(
+      CORSMiddleware,
+      allow_origins=["*"],
+      allow_credentials=True,
+      allow_methods=["*"],
+      allow_headers=["*"],
+)
