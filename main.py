9e80bd853f1128bf0c6db33bc846f152e84603b8
kamela <kamelawaelelgadi889@gmail.com>
2025-10-02 03:23:40 +0100

    nnew

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from routers import register ,login
 
 app = FastAPI()
-app.include_router(register.router)
-app.include_router(login.router)
+#app.include_router(register.router)
+#app.include_router(login.router)
 @app.get("/")
 def home():
    return {"message":"welcom to my API 🎉"}
