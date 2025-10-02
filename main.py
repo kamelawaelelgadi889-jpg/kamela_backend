7cf817a871e157cfb9d16c465220274d59d129c7
kamela <kamelawaelelgadi889@gmail.com>
2025-10-02 20:40:50 +0100

    اخر تعديل ان شاء الله

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from routers import register ,login
 from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 app.include_router(register.router)
 app.include_router(login.router)
+@app.get("/")
+def root():
+    return {"message": "welcom to my api"}
 app.add_middleware(
       CORSMiddleware,
       allow_origins=["*"],
